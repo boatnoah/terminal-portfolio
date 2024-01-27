ce77b6bc66799ff70932b7f6cc722e84a7080811
boatnoah <noahskim7@gmail.com>
2024-01-27 00:14:03 -0800

    added project files
    Added project files and wrote commands for the terminal use.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # terminal-portfolio
+
+A website with a terminal GUI and functionality.
+Check it out live here:
---
+++ commands.js
@@ -0,0 +1,41 @@
+const linkedin = "https://www.linkedin.com/in/noah-kim-cs/";
+const tiktok = "https://www.tiktok.com/@csboat?_t=8jNKJzYnmVm&_r=1";
+const github = "https://github.com/boatnoah";
+const resume = "https://www.resume.lol/share/lb03iae";
+const email = "noahkimCS@gmail.com";
+
+help = [
+  "aboutme        - Get to know me!",
+  "projects       - See the latest projects I've built",
+  "socials        - Displays my socials",
+  "linkedin       - Check out my LinkedIn",
+  "tiktok         - Check out my TikTok",
+  "github         - My GitHub profile",
+  "resume         - Check out my resume",
+  "email          - Shoot me an email",
+  "clear          - Clear Terminal",
+  "chess          - Coming Soon...",
+];
+
+aboutme = [
+  "Hey, I'm Noah!",
+  "I'm currently a 2nd-year student at California State University, Long Beach",
+  "studying Computer Science. I enjoy coding, teaching others to code, and learning more about it.",
+  "So for the past year, I have been working as a coding coach teaching kids aged 8-12",
+  "the basics of programming. As a coach, I focus on developing projects that I have made into small",
+  "coding lessons and ensuring each child progresses in their programming.",
+  "I'm excited to be on this journey of growth and development, and",
+  "I can't wait to see what the future holds in this rapidly evolving field.",
+];
+
+projects = [
+  `<a href="https://boatnoah.github.io/Pathfinding-Project/" target="_blank">Pathfinding Visualizer</a> - Visualize how popular pathfinding algorithms work!`,
+  `<a href="https://github.com/boatnoah/Chess-Game-Project" target="_blank">Chess</a> - Play a fun chess game!`,
+  `<a href="https://github.com/boatnoah/TicTacToeAI-Django" target="_blank">Tic-Tac-Toe AI</a> - See how AI has solved Tic-Tac-Toe!`,
+];
+
+socials = [
+  `LinkedIn          <a href="${linkedin}"> target="_blank">linkedin/noah-kim-cs</a>`,
+  `TikTok            <a href="${tiktok}"> target="_blank">tiktok/csboat</a>`,
+  `GitHub            <a href="${github}"> target="_blank">tiktok/csboat</a>`,
+];
